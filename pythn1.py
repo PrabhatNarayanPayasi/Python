7c774a21fdc07477f7a8737897c7df836fbdad18
Prabhat Narayan Payasi <142197070+PrabhatNarayanPayasi@users.noreply.github.com>
2025-05-07 06:50:12 +0530

    Add files via upload

---
+++ pythn1.py
@@ -0,0 +1,36 @@
+# print("Prabhat");
+# print("Hello World")
+# a=2;
+# b=2;
+# print(a+b)
+# print(a*b)
+print(2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Python ek high-level, interpreted programming language hai jo easy-to-read syntax ke liye jaana jaata hai. 
+Yeh general-purpose language hai, jise web development, data analysis, artificial intelligence, automation, aur scientific computing mein use kiya jata hai.
+Python ki popularity kaafi badh gayi hai kyunki isme code likhna simple hota hai aur bohot saare powerful libraries aur frameworks available hote hain.
+'''
