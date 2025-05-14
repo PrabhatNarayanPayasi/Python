00d6ba3b9689a6675afce762ccb2cc309b4c416a
Prabhat Narayan Payasi <142197070+PrabhatNarayanPayasi@users.noreply.github.com>
2025-05-15 03:25:47 +0530

    Add files via upload

---
+++ Day8-9.py
@@ -0,0 +1,9 @@
+# Exercise
+
+a = 8;
+b = 8;
+print(a+b);
+print(a-b);
+print(a*b);
+print(a%b); # modulus shows what is the reminder 
+print(type(a/b)); # it shows how many times number is got devided .  it always return float tye value . 
