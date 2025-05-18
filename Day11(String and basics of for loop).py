3ab488c187200f182660606caaa6fde0dc15cf14
Prabhat Narayan Payasi <142197070+PrabhatNarayanPayasi@users.noreply.github.com>
2025-05-18 07:47:09 +0530

    Add files via upload

---
+++ Day11(String and basics of for loop).py
@@ -0,0 +1,96 @@
+# name="Prabhat"
+# details = "HII" , "kon" , 2, 3, 5,50
+# print(name[0])
+# print(name[1])
+'''print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])'''
+ 
+
+# print(details)
+# print(details[0[1]])
+# print(details[1])
+# for c  in name :
+#     print(c)
+
+apple= '''Hello i \"ArithmeticError\" \n
+akdfd
+sdf
+sorted'''
+print(apple)
+
+
+
+'''
+
+What are strings?
+In python, anything that you enclose between single or double quotation marks is considered a string. 
+A string is essentially a sequence or array of textual data. Strings are used when working with Unicode characters.
+
+Example
+name = "Harry"
+print("Hello, " + name)
+Output
+Hello, Harry
+
+Note: It does not matter whether you enclose your strings in single or double quotes, the output remains the same.
+
+Sometimes, the user might need to put quotation marks in between the strings. Example, consider the sentence: He said, “I want to eat an apple”.
+
+How will you print this statement in python?: He said, "I want to eat an apple". We will definitely use single quotes for our convenience
+
+print('He said, "I want to eat an apple".')
+Multiline Strings
+If our string has multiple lines, we can create them like this:
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+Accessing Characters of a String
+
+
+In Python, string is like an array of characters.
+We can access parts of string by using its index which starts from 0.
+Square brackets can be used to access elements of the string.
+
+print(name[0])
+print(name[1])
+Looping through the string
+We can loop through strings using a for loop like this:
+
+for character in name:
+    print(character)
+Above code prints all the characters in the string name one by one!
+
+'''
+
+'''str = """wwwwwwwwwwwwwwww
+wwwwwwwwwwww
+wwwwwwwwwwwwww
+wwwwwwwwwwww
+wwwwwwwwwww"""
+print(str)
+ 
+for i in range(10):
+    print("I am ok" , i)'''
+# print("hii \n kaise ho")
+# print("hello \"pp\"")
+# name = ("Hello" ,  "Hii" , "name" ,"yours");
+# n = len(name)
+# for i in range(n):
+#     print(name[i])
+# print("hello","HI")
+
+
+print("""sd
+      sorteds
+      sep=sep=
+      sep=SystemError
+      """) # this is used for multiline comment . 
+
+for i in range(0 ,5):
+    print(i)
