af25fa32d38870e7d6869daf6f625968908f5bd5
Prabhat Narayan Payasi <142197070+PrabhatNarayanPayasi@users.noreply.github.com>
2025-05-21 07:12:17 +0530

    Add files via upload

---
+++ Day14(If  else Conditional Statement).py
@@ -0,0 +1,21 @@
+'''a = int(input("Enter number : "));
+print("Your enterd number is : " , a);
+print(a<=18); # Ye true retutrn karega if thte condition goes match.
+print(a>=18);
+print(a!=18);
+print(a==18);
+print(a==4);
+
+if(a>18):
+    print("A is greater than 18");
+    print("Okk");
+else:
+    print("Not graeter");
+print("Yees !!");'''
+a = int(input("Enter the number : "));
+print(a==18);
+print(a>18)
+if(a==18):
+    print("Number 18 hai")
+elif(a==19):
+    print("Age is 19")
