ba6637cc1d377583382bf21fffdedcc2164e9f7c
Prabhat Narayan Payasi <142197070+PrabhatNarayanPayasi@users.noreply.github.com>
2025-05-28 21:59:23 +0530

    Add files via upload

---
+++ Day17.py
@@ -0,0 +1,36 @@
+# ------------------------------------------------------ For LOOPS ---------------------------------------------------------------------------
+'''
+name = "Prabhat"
+for i in  name:
+    print(i);
+    if(i=="a"):
+        print("a aa gaya.");
+        # break;
+        continue;
+lists = ["Array" , "Prabhat" , "Shiva" , "Apple" , "Mangoes" , "Narayan"]
+for list in lists:
+        print(list);
+        for each in list:x
+            print(each);
+         
+for i in range(-1 , 10):
+     print(i);'''
+     
+# for i in range(1 , 12 , 3):
+#          print(i);
+# x =int(input("Enter x :" ));
+# for i in range(10):
+#     print((i+1)*x);
+
+x = int(input("Enter num"));
+for i in range(x):
+    print("This is index ", i);
+    if(i==4):
+        print("Got IT");
+        
+    if(i==2):
+        print("Pahuche kya");
+        # break;
+        continue;
+    
+    
